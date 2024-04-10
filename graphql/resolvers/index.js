4c1e0f03f74962a4fdc735edad1eb2b9250fc102
Alexandr Babac <alexandr.babac@amdaris.com>
2024-04-10 14:42:57 +0300

    added products data

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,10 +1,13 @@
 const usersResolvers = require('./users');
 const featureToggleResolvers = require('./featureToggles');
+const productsResolvers = require('./products')
 
 module.exports = {
     Query: {
         ...usersResolvers.Query,
-        ...featureToggleResolvers.Query
+        ...featureToggleResolvers.Query,
+        ...productsResolvers.Query
+
     },
     Mutation: {
         ...usersResolvers.Mutation,
